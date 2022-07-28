7b91f8c5274568e2f84479570270245de08d742e
MinJ <m04j00@gmail.com>
2022-07-28 15:32:59 +0900

    feat : 글 보기 기능 구현
    - 파라미터의 id에 해당하는 글 불러오는 작업 완료
    - UI 작업 진행 예정

--- src/components/ListDetail.js
+++ src/components/ListDetail.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import dummy from "../data/data.json";
 
 const ListDetail = () => {
     const { id } = useParams();
-
+    const post = dummy.filter(item => {
+        return item.id == id;
+    })
     return(
         <div>
-                <h1>ListDetail {id}</h1>
+            <h1>{post[0].title}</h1>
+            <p>{post[0].body}</p>
         </div>
-    )
+    );
 
 }
 
