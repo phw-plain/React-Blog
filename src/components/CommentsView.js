72dfb9c94781b471b5c7d80c07e43234b0c86d27
plainphw <s2028@e-mirim.hs.kr>
2022-07-28 15:13:59 +0900

    Update 댓글 좋아요 기능 추가

--- src/components/CommentsView.js
+++ src/components/CommentsView.js
@@ -1,27 +1,45 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 import comments from '../data/comments.json'
 
 const CommentsView = (props) => {
     const [comment, setComment] = useState(comments);
 
+    const countLikt = (idx) => {
+        if(document.body.querySelector(`#like${idx}`).classList[1] !== 'on'){
+            const newComment = [...comment];
+            newComment.map((item, index) =>  {
+                if(item.id === Number(props.id) && idx === index) {
+                    newComment[index].like++;
+                    setComment(newComment)
+                    document.body.querySelector(`#like${idx}`).classList.add('on')
+                }
+            })
+        }
+    }
+
     return(
         <div id="commentsview">
             {
-                comment.filter(item => item.id === Number(props.id)).map((item, idx) => 
-                    <div className='comment' key={idx}>
-                        <div className='inline'>
-                            <div>😀</div>
-                            <div className='comment-name'>{item.name}</div>
-                        </div>
-                        <div className='left'>
-                            {item.body}
-                        </div>
-                    </div>
+                comment.map((item, idx) => 
+                    (item.id === Number(props.id)) ? 
+                        <div className='comment' key={idx}>
+                            <div className='inline'>
+                                <div>😀</div>
+                                <div className='comment-name'>{item.name}</div>
+                            </div>
+                            <div className='flex-between'>
+                                <div>{item.body}</div>
+                                <button id={`like${idx}`} className='likebtn' onClick={ () => countLikt(idx) }>❤ {item.like}</button>
+                            </div>
+                        </div> 
+                         : null
                 )
             }
         </div>
     )
 }
+
+
 
 export default CommentsView;
